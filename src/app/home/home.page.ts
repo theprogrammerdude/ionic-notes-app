a7dcb192b4ba76772e53c9ebe99d1ffcf43b2141
TheProgrammerDude <shas19980805@gmail.com>
2020-09-26 22:23:52 +0530

    fully functional app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  note: string = "";
+  notes: any[] = [];
 
   constructor() {}
 
+  ionViewWillEnter() {
+    if (localStorage.getItem("notes")) {
+      this.notes = JSON.parse(localStorage.getItem("notes"));
+    }
+  }
+
+  addNote() {
+    if (this.note != "") {
+      let data = { note: this.note };
+      this.notes.push(data);
+      localStorage.setItem("notes", JSON.stringify(this.notes));
+    }
+
+    this.note = "";
+  }
+
+  deleteNote(i: number) {
+    this.notes.splice(i, 1);
+    localStorage.setItem("notes", JSON.stringify(this.notes));
+  }
+
+  deleteAll() {
+    this.notes = [];
+    localStorage.setItem("notes", JSON.stringify(this.notes));
+  }
 }
